e7411b6e3dc9abba77352181c6c0e37538e116cd
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-10-12 17:00:10 -0500

    control flow

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,6 +1,28 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
         int i = 5;
-    // comment
+        while(i < 15){
+            System.out.println(i);
+            i++;
+        }
+            System.out.println(i);
+
+        int j = 0;
+         do {
+         j+=2;
+         System.out.println(j);
+         } while (j < 100);
+
+        int k = 100;
+        do {
+            k-=5;
+            System.out.println(k);
+        } while (k > -10 );
+
+        long l = 2;
+        do {
+            l= l * l;
+            System.out.println(l);
+        } while (l < 1000000);
     }
 }
