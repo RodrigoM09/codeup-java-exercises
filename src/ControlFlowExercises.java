95eb3c31b61ea654d7461a6a314cdf4a6b7d03a5
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-10-13 15:32:45 -0500

    string lecture

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,12 @@
-import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
         int i = 5;
-        while(i < 15){
+        while(i <= 15){
             System.out.println(i);
             i++;
         }
-            System.out.println(i);
 
         int j = 0;
          do {
@@ -17,19 +15,28 @@ public class ControlFlowExercises {
          } while (j < 100);
 
         int k = 100;
+
         do {
             k-=5;
             System.out.println(k);
         } while (k > -10 );
 
         long l = 2;
+
         do {
-            l= l * l;
             System.out.println(l);
+            l= l * l;
         } while (l < 1000000);
 
+        for (int m = 5; i<=15; m++){
+            System.out.println(m);
+        }
 
-        for (int r = 0; r <= 100; r++) {
+        for (int n =100; n >= -10; n-=5){
+            System.out.println(n);
+        }
+
+        for (int r = 1; r <= 100; r++) {
             if  (r % 3 == 0 & r % 5 == 0){
                 System.out.println("FizzBuzz");
             } else if (r % 5 == 0) {
@@ -47,8 +54,8 @@ public class ControlFlowExercises {
             System.out.println("Enter a number: ");
             int userNum = scanner.nextInt();
 
-            System.out.println("Number" + "|" + "Squared" + "|" + "Cubed");
-            System.out.println("------" + "|" + "------" + "|" + "------");
+            System.out.println("Number | Squared | Cubed");
+            System.out.println("------ | ------ | ------");
             for (int s = 1; s <= userNum; s++) {
                 s = userNum;
                 int numberSquared = (int) Math.pow(s, 2);
@@ -56,7 +63,6 @@ public class ControlFlowExercises {
 
                 String message = "\n" + s + "      " + numberSquared + "      " + numberCubed;
                 System.out.println(message);
-                System.out.println();
 
             }   // see if the user wants to continue
             System.out.print("Continue? (y/n): ");
@@ -71,13 +77,13 @@ public class ControlFlowExercises {
         int userGrade = scanner.nextInt();
         if (userGrade <= 59){
             System.out.println("You're a failure");
-        } else if ((userGrade >= 60) & (userGrade <= 66)){
+        } else if (userGrade <= 66){
             System.out.println("Your letter grade is a D");
-        } else if ((userGrade >= 67) & (userGrade <= 79)){
+        } else if (userGrade <= 79){
             System.out.println("Your letter grade is a C");
-        } else if ((userGrade >= 80) & (userGrade <= 87)) {
+        } else if (userGrade <= 87) {
             System.out.println("Your letter grade is a B");
-        } else if ((userGrade >= 88) & (userGrade <= 100)) {
+        } else if (userGrade <= 100) {
             System.out.println("Your letter grade is a A");
         }
             System.out.print("Continue? (y/n): ");
---
+++ src/StringsLecuture.java
@@ -0,0 +1,5 @@
+public class StringsLecuture {
+    public static void main(String[] args) {
+
+    }
+}
