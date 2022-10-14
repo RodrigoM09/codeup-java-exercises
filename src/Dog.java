c5fe621c93c386f179b918e59b274a54b8575f02
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-10-14 14:10:48 -0500

    dog.java

--- src/Dog.java
+++ src/Dog.java
@@ -1,9 +1,39 @@
 public class Dog {
-    public static void bark(){
-        System.out.println("Woof Woof!");
+    public static String bark(){
+//        System.out.println("Woof Woof!");
+        return "Woof Woof";
     }
+
+
+    public static void bark(int weight){
+      if (weight < 25){
+          System.out.println("Yip Yip");
+      }else{
+          System.out.println("Woof Woof!");
+      }
+    }
+
+    public static void bark(int weight, String name ){
+        System.out.printf("%s goes ", name);
+        bark(weight);
+//        String output = name + " goes ";
+//        if (weight < 25){
+//            output += "yip yip";
+//        }else {
+//           output += "woof woof";
+//        }
+//        System.out.println(output);
+    }
+
     public static void main(String[] args) {
-        bark();
+        System.out.println(bark());
+
+
+        //bark();
+//        bark(15);
+//        bark(50);
+//        bark(50, "Rudo");
+//        bark(15,"Fido");
 
     }
 }
